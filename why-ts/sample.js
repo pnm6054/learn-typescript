cbf9da7aef160bb771ea1c9f1aef3626be920790
김선준 <sjkim@miridih.com>
2022-11-20 16:55:08 +0000

    ~session1

---
+++ why-ts/sample.js
@@ -0,0 +1,19 @@
+// function sum(a, b) {
+//     return a + b;
+// }
+// // sum(10, '20'); // 1020
+// var result1 = sum(10, 20);
+// result.toLocaleString();
+
+// @ts-check <-- 이거 있으면 js 파일에서 ts 처럼 검사
+
+/**
+ * 
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ */
+function sum(a, b) {
+    return a + b;
+}
+
+// sum(10, '20'); // 이거 잘못됐다고 알려주는건 ts만 가능
